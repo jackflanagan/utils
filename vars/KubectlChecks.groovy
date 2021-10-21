2e44380d8aebe6bf6797cb79022d2702d2438a08
Jack Flanagan <jackjohnflanagan@hotmail.co.uk>
2021-10-21 10:55:10 +0100

    testing library

--- vars/KubectlChecks.groovy
+++ vars/KubectlChecks.groovy
@@ -1,13 +1,3 @@
 def CheckNodes(){
-    NotReadyNodes = sh(
-        script: 'kubectl get nodes | grep NotReady',
-        returnStdout: true
-    )
-    if(NotReadyNodes.isEmpty()){
-        echo "No Nodes in NotReady status at time of running"
-    } else {
-        echo "Nodes in NotReady state"
-        sh 'kubectl get nodes'
-        exit 0 
-    }
+    echo "Testing"
 }
