6bf6da250c338e09fa2f9d57318c1d61f9212f56
Jack Flanagan <jackjohnflanagan@hotmail.co.uk>
2021-10-21 11:29:59 +0100

    adding kube bits

--- vars/kubechecks.groovy
+++ vars/kubechecks.groovy
@@ -1,3 +1,13 @@
 def addtest(){
-    echo "2 + 2 = 4"
+    NotReadyNodes = sh(
+        script: 'kubectl get nodes | grep NotReady'
+        returnStdout: true
+    )
+    if(NotReadyNodes.isEmpty()){
+        echo "No Nodes in NotReady status at time of running"
+    } else {
+        echo "Nodes in NotReady state"
+        sh 'kubectl get nodes'
+        exit 0 
+    }
 }
