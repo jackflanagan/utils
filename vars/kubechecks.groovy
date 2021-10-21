4a33669365387ff8681af54033eafd5c305264c0
Jack Flanagan <jackjohnflanagan@hotmail.co.uk>
2021-10-21 11:39:32 +0100

    adding kube bits

--- vars/kubechecks.groovy
+++ vars/kubechecks.groovy
@@ -5,7 +5,7 @@ def addtest(){
     )
     echo "Pointer 1"
     echo NotReadyNodes
-    if(NotReadyNodes.isEmpty()){
+    if(NotReadyNodes=1){
         echo "No Nodes in NotReady status at time of running"
     } else {
         echo "Nodes in NotReady state"
